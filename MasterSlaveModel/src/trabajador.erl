1de508dea893633189858b46abe25a643138e133
jalbertcruz <jalbertcruz@gmail.com>
2011-11-10 18:44:57 -0700

    Incorporada la licencia.

--- MasterSlaveModel/src/trabajador.erl
+++ MasterSlaveModel/src/trabajador.erl
@@ -1,3 +1,16 @@
+%% 
+%% Author José Albert Cruz Almaguer <jalbertcruz@gmail.com>
+%% Copyright 2011 by José Albert Cruz Almaguer.
+%% 
+%% This program is licensed to you under the terms of version 3 of the
+%% GNU Affero General Public License. This program is distributed WITHOUT
+%% ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+%% MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+%% AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+%% 
+%% 
+%% @doc Módulo que implementa el comportamiento de los trabajadores.
+%% 
 -module(trabajador).
 -compile(export_all).
 
@@ -13,6 +26,7 @@ init(Fitness)->
 loop(Fitness) ->
   receive
 
+      %% Mensaje que le ordena a un trabajador realizar el cálculo.
       {calcula, Cromosoma} ->
 	  colector ! {calculado, Cromosoma, Fitness(Cromosoma), self()},
           loop(Fitness);
