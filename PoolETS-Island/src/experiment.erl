ea76253f42eb1cae1a99a86395a5f90aea40eff8
J. Albert Cruz <jalbertcruz@gmail.com>
2013-05-19 10:24:37 -0400

    refactor MAEB.

--- PoolETS-Island/src/experiment.erl
+++ PoolETS-Island/src/experiment.erl
@@ -8,7 +8,7 @@
 %% MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
 %% AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 %% 
--module(e).
+-module(experiment).
 -author("jalbertcruz@gmail.com").
 
 -include("../include/mtypes.hrl").
